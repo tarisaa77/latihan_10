d57932df567fdbb69920a74c90d6265821778179
Tina <tinaatrsaa@gmail.com>
2025-08-20 11:32:53 +0700

    SDK Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.github.bumptech.glide:glide:4.16.0")
